56e6c4f9d6616bc4db3e3ba1217784c0a1d55668
Omkar Kulkarni <omkarak45@gmail.com>
2020-10-08 20:24:33 +0530

    Imported mongoose models

--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const Blog = require("../models/Blog");
 
-router.get('/', (req,res)=>{
-    res.json({msg:'I\'m a blog route'})
-})
+router.get("/", (req, res) => {
+  res.json({ msg: "I'm a blog route" });
+});
 
-
-module.exports = router
+module.exports = router;
