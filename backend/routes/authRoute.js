188d4f35b15483fb93db1b239ae0837728d5d4db
Omkar Kulkarni <omkarak45@gmail.com>
2020-10-09 14:04:33 +0530

    Added method for registering new user and saving in db

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,16 +1,49 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
-router.get("/login", (req, res) => {
-  res.json({
-    msg: "Login Route",
-  });
-});
+router.get("/login", (req, res) => {});
 
 router.get("/register", (req, res) => {
   res.json({
     msg: "Registration route",
   });
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, name, email, password } = req.body;
+
+    const result = await User.findOne({
+      email: email,
+    });
+    if (result) {
+      throw err;
+    } else {
+      const newUser = new User({
+        username,
+        name,
+        email,
+        password,
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (error, hash) => {
+          if (error) throw error;
+
+          newUser.password = hash;
+
+          newUser.save().then((user) => {
+            console.log("Registration succeed.");
+            res.json({msg:"User registered."})
+          });
+        });
+      });
+    }
+  } catch (error) {
+    res.json({ error: "Some Error occured." });
+  }
 });
 
 module.exports = router;
