ea16c353c5ced1c6da289cf92530f815c0c9e4ac
Omkar Kulkarni <omkarak45@gmail.com>
2020-10-11 17:50:31 +0530

    UI Fixed on newBlog component.

--- frontend/src/components/pages/NewBlog/NewBlog.component.jsx
+++ frontend/src/components/pages/NewBlog/NewBlog.component.jsx
@@ -12,21 +12,26 @@ const NewBlog = () => {
   };
   return (
     <React.Fragment>
-      <Flex flexDirection="column" maxW="1200px" margin="0 auto">
+      <Flex flexDirection="column" maxW={['90%', '1200px']} padding='0.5rem 1rem' border='1px solid #eee' borderRadius='10px' margin="1rem auto">
         <form onSubmit={handleSubmit}>
-          <Input />
+          <Heading margin='1rem 0'>New Blog</Heading>
+          <Input margin='1rem 0' placeholder='New blog title here...'/>
           <Box></Box>
           <MDEditor height={400} value={value} onChange={setValue} />
-          <Flex>
+          <Flex  margin='1rem 0'>
             <Button
               backgroundColor={theme.colors.accent}
               color="white"
+             
               _hover={{ color: "white", backgroundColor: "#323EBE" }}
               type='submit'
             >
               Publish
             </Button>
             {/* Future update : Save draft feature */}
+            <Button marginLeft='2rem' isDisabled={true}>  
+              Save as a draft
+            </Button>
           </Flex>
         </form>
       </Flex>
