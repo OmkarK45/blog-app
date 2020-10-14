d7a7dcf6c797e0345ea0ce0306e3a3c8b5edc900
Omkar Kulkarni <omkarak45@gmail.com>
2020-10-14 17:47:52 +0530

    [Dark Mode]Initial support for Dark Mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
 
 ReactDOM.render(
   <ThemeProvider>
