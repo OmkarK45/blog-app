5384953b639b5da4966f037aed4e82ce599e8cce
Omkar Kulkarni <omkarak45@gmail.com>
2020-10-16 19:05:54 +0530

    Added delete button for only those who own the blog

--- frontend/src/components/common/AuthorMenu/AuthorMenu.component.jsx
+++ frontend/src/components/common/AuthorMenu/AuthorMenu.component.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useContext } from "react";
 import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    MenuGroup,
-    MenuDivider,
-    MenuOptionGroup,
-    MenuItemOption,
-  } from "@chakra-ui/core";
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuGroup,
+  MenuDivider,
+  MenuOptionGroup,
+  MenuItemOption,
+  Text,
+} from "@chakra-ui/core";
+import Delete from "./DeleteButton.component";
+import userContext from "../../../context/userContext";
 
 const AuthorMenu = (props) => {
-    return ( <h1>Menu here</h1> );
-}
- 
+  const { userData } = useContext(userContext);
+  console.log("Current user", userData.user.id);
+  console.log("Blogs author", props.blogInfo.authorID);
+  return (
+    <React.Fragment>
+      {props.blogInfo ? (
+        props.blogInfo.authorID === userData.user.id ? (
+          <Delete data={props} />
+        ) : (
+          "Not Author no delete button for you."
+        )
+      ) : (
+        "no blog is there"
+      )}
+    </React.Fragment>
+  );
+};
+
 export default AuthorMenu;
