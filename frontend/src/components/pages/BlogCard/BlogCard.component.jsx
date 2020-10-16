f1ce9838136b94e71c06355c798eb167844523e6
Omkar Kulkarni <omkarak45@gmail.com>
2020-10-16 19:07:32 +0530

    Removed props passing. No need now.

--- frontend/src/components/pages/BlogCard/BlogCard.component.jsx
+++ frontend/src/components/pages/BlogCard/BlogCard.component.jsx
@@ -30,81 +30,75 @@ const BlogCard = (props) => {
   }, []);
 
   return (
-   
-      <Link
-        to={{
-          pathname: `/blogs/${props.data.authorID}/${props.data._id}`,
-          state: { ...{ props }, user:userData },
-        }}
+    <Link to={`/blogs/${props.data.author}/${props.data._id}`}>
+      <Box
+        maxH="400px"
+        margin="1rem 0"
+        border="1px solid #eee"
+        overflow="hidden"
+        borderRadius="10px"
+        backgroundColor="white"
+        boxShadow="0 0 0 1px rgba(8,9,10,0.1)"
+        cursor="pointer"
       >
-        <Box
-          maxH="400px"
-          margin="1rem 0"
-          border="1px solid #eee"
-          overflow="hidden"
-          borderRadius="10px"
-          backgroundColor="white"
-          boxShadow="0 0 0 1px rgba(8,9,10,0.1)"
-          cursor="pointer"
-        >
-          <Box>
-            <Flex direction="column">
-              <Box overflow="hidden">
-                {!image && props.data.bannerURL && (
-                  <Skeleton height="200px" width="100%" />
-                )}
-                <Image
-                  src={props.data.bannerURL ? props.data.bannerURL : ""}
-                  height={!image ? "" : "200px"}
-                  width="100%"
-                  objectFit="cover"
-                  className="blog-banner"
-                  onLoad={handleImageLoad}
-                />
-              </Box>
+        <Box>
+          <Flex direction="column">
+            <Box overflow="hidden">
+              {!image && props.data.bannerURL && (
+                <Skeleton height="200px" width="100%" />
+              )}
+              <Image
+                src={props.data.bannerURL ? props.data.bannerURL : ""}
+                height={!image ? "" : "200px"}
+                width="100%"
+                objectFit="cover"
+                className="blog-banner"
+                onLoad={handleImageLoad}
+              />
+            </Box>
 
-              <Flex direction="column" padding="1rem 1.5rem">
-                <Heading
-                  fontSize={["1.5rem", "1.8rem"]}
-                  fontFamily={theme.fonts.heading}
-                  fontWeight={theme.fontWeights.bold}
-                >
-                  {/* Add Link tag surrounding this. */}
-                  {props.data.title}
-                </Heading>
-                <Box marginTop="0.7rem">
-                  <Flex alignItems="center">
-                    <Box borderRadius="50%">
-                      {props.data.avatar ? (
-                        <Box
-                          width="32px"
-                          height="32px"
-                          borderRadius="50%"
-                          overflow="hidden"
-                        >
-                          <Image src={props.data.avatar} />
-                        </Box>
-                      ) : (
-                        <HiOutlineUserCircle size="32px" />
-                      )}
-                    </Box>
-                    <Flex>
-                      <Text
-                        color="#4d5760"
-                        fontFamily={theme.fonts.heading}
-                        marginLeft="0.7rem"
+            <Flex direction="column" padding="1rem 1.5rem">
+              <Heading
+                fontSize={["1.5rem", "1.8rem"]}
+                fontFamily={theme.fonts.heading}
+                fontWeight={theme.fontWeights.bold}
+              >
+                {/* Add Link tag surrounding this. */}
+                {props.data.title}
+              </Heading>
+              <Box marginTop="0.7rem">
+                <Flex alignItems="center">
+                  <Box borderRadius="50%">
+                    {props.data.avatar ? (
+                      <Box
+                        width="32px"
+                        height="32px"
+                        borderRadius="50%"
+                        overflow="hidden"
                       >
-                        {props.data.author} &nbsp; • {dateVar}
-                      </Text>
-                      {/* Future Update : Date in MM/DD */}
-                    </Flex>
+                        <Image src={props.data.avatar} />
+                      </Box>
+                    ) : (
+                      <HiOutlineUserCircle size="32px" />
+                    )}
+                  </Box>
+                  <Flex>
+                    <Text
+                      color="#4d5760"
+                      fontFamily={theme.fonts.heading}
+                      marginLeft="0.7rem"
+                    >
+                      {props.data.author} &nbsp; • {dateVar}
+                    </Text>
+                    {/* Future Update : Date in MM/DD */}
                   </Flex>
-                </Box>
-              </Flex>
+                </Flex>
+              </Box>
             </Flex>
-          </Box>
+          </Flex>
         </Box>
-      </Link>
+      </Box>
+    </Link>
   );
 };
 
