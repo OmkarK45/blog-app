d7e0dbba09e99eab6cdfaadfa94013f15cc45406
Omkar Kulkarni <omkarak45@gmail.com>
2020-10-17 22:26:00 +0530

    Improved 404 Page :)

--- frontend/src/components/pages/404/NotFound.component.jsx
+++ frontend/src/components/pages/404/NotFound.component.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Heading, Text, Button, Flex } from "@chakra-ui/core";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    return ( <h1>Not Found</h1> );
-}
- 
+  return (
+    <React.Fragment>
+      <Flex
+        height="50vh"
+        alignItems="center"
+        flexDirection="column"
+        justifyContent="center"
+      >
+        <Heading>404!</Heading>
+        <Heading>
+          Oops ! Requested resource was not found on this website.
+        </Heading>
+        <Link to="/blogs" style={{ marginTop: "5rem" }}>
+          <Button>Go Back</Button>
+        </Link>
+      </Flex>
+    </React.Fragment>
+  );
+};
+
 export default NotFound;
