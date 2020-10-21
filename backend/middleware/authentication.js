cdfc3fbf4b6276889281a51afd58a858c6f6e37a
Omkar Kulkarni <omkarak45@gmail.com>
2020-10-21 22:15:12 +0530

    Nothing changed

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -2,8 +2,7 @@ const jwt = require("jsonwebtoken");
 
 function authentication(req, res, next) {
   const token = req.header("x-auth-token");
-  if (!token) res.status(401).json({ msg: "Auth Token not found." });
-
+  if (!token) return res.status(401).json({ msg: "Auth Token not found." });
   try {
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decodedToken;
