80d8d0b7690fd0cf7602255b7104e814e68f6beb
Omkar Kulkarni <omkarak45@gmail.com>
2020-10-28 20:01:09 +0530

    Social Media Component

---
+++ frontend/src/components/common/SocialMedia/SocialMedia.component.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { AiFillTwitterCircle, AiFillLinkedin } from "react-icons/ai";
+import { FaReddit } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { Flex, Box } from "@chakra-ui/core";
+
+const SocialMedia = (props) => {
+  return (
+    <Flex
+      direction={["row", "row", "row", "column"]}
+      margin="0 auto"
+      width="80%"
+      alignItems={["center", "flex-end"]}
+      justifyContent="space-between"
+      marginTop={["0", "0", "0", "2rem"]}
+    >
+      <Box margin="0.5rem 0">
+        <a href={props.link}>
+          <AiFillTwitterCircle style={{ color: "#1da1f2" }} size="45px" />
+        </a>
+      </Box>
+      <Box margin="0.5rem 0">
+        <a href={props.link}>
+          <FaReddit style={{ color: "#ff4500" }} size="45px" />
+        </a>
+      </Box>
+      <Box margin="0.5rem 0">
+        <a href={props.link}>
+          <IoLogoWhatsapp style={{ color: "#25d366" }} size="45px" />
+        </a>
+      </Box>
+      <Box margin="0.5rem 0">
+        <a href={props.link}>
+          <AiFillLinkedin style={{ color: "#0077b5" }} size="45px" />
+        </a>
+      </Box>
+    </Flex>
+  );
+};
+
+export default SocialMedia;
